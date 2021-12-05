d3d16594d8f5aeef4ec72c64945d226b53bf1299
Remi Rampin <remi@rampin.org>
2021-12-05 10:25:39 -0500

    Remove loading state from GamesList
    Can't happen, the App won't render until the games have been loaded.

--- src/GamesList.tsx
+++ src/GamesList.tsx
@@ -8,21 +8,13 @@ interface GamesListProps {
 
 export class GamesList extends React.PureComponent<GamesListProps> {
   render() {
-    if(this.props.games === null) {
-      return (
-        <div className="App">
-          <p>Loading...</p>
-        </div>
-      );
-    } else {
-      return (
-        <ul className="games-list">
-          {this.props.games.map((game) => (
-            <li key={game.id}><Link to={`/${game.id}`}>{game.name}</Link></li>
-          ))}
-          <li key="new" className="new-game"><Link to="/new">New game</Link></li>
-        </ul>
-      );
-    }
+    return (
+      <ul className="games-list">
+        {this.props.games.map((game) => (
+          <li key={game.id}><Link to={`/${game.id}`}>{game.name}</Link></li>
+        ))}
+        <li key="new" className="new-game"><Link to="/new">New game</Link></li>
+      </ul>
+    );
   }
 }
